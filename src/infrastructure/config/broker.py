5c2828d7132538e1694505f5ffbd571e136be0d6
sxkxixx <sasha.kornilov.1212@gmail.com>
2024-03-07 21:56:00 +0500

    Добавил дефолтные значения для конфигов брокера

--- src/infrastructure/config/broker.py
+++ src/infrastructure/config/broker.py
@@ -6,7 +6,7 @@ dotenv.load_dotenv()
 
 
 class BrokerConfig:
-    BROKER_HOST: str = os.getenv('BROKER_HOST')
-    BROKER_PORT: int = int(os.getenv('BROKER_PORT'))
-    BROKER_USER: str = os.getenv('BROKER_USER')
-    BROKER_PASSWORD: str = os.getenv('BROKER_PASSWORD')
+    BROKER_HOST: str = os.getenv('BROKER_HOST', 'localhost')
+    BROKER_PORT: int = int(os.getenv('BROKER_PORT', 5672))
+    BROKER_USER: str = os.getenv('BROKER_USER', 'guest')
+    BROKER_PASSWORD: str = os.getenv('BROKER_PASSWORD', 'guest')
